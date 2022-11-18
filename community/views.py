04c3d07fed262ea4e96e79e447e6db3f4e8c1b36
dippola <dippolas@gmail.com>
2022-11-18 12:55:14 +0900

    version4

--- community/views.py
+++ community/views.py
@@ -37,7 +37,8 @@ def updateUser(request, uid):
     serializer = UserModel_serializer(user, data=data)
     if serializer.is_valid():
         serializer.save()
-    return Response(serializer.data)
+        return Response(serializer.data)
+    return Response(serializer.errors)
 
 
 
