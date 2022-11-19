54168bb01e4b51d14cfa8ff627d247f680090915
dippola <dippolas@gmail.com>
2022-11-19 17:14:17 +0900

    version19

--- community/urls.py
+++ community/urls.py
@@ -26,7 +26,7 @@ urlpatterns = [
     path('posts/', views.getMains),
     path('posts/page=<str:page>/', views.getMainsPage),
     path('post/create/', views.createMain),
-    path('post/<str:pk>', views.getMain),
+    path('post/<str:pk>/', views.getMain),
     path('post/<str:pk>/update/', views.updateMain),
     path('post/<str:pk>/delete/', views.deleteMain),
     path('post/<str:pk>/comment/create=<str:uid>/', views.createMainComment),
