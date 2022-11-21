5d614be049ba1a49589d43cade55a4848a05b739
dippola <dippolas@gmail.com>
2022-11-21 18:03:44 +0900

    version44

--- community/views.py
+++ community/views.py
@@ -159,7 +159,7 @@ def updateMainComment(request, id):
     serializer = MainCommentModel_serializer(comment, data=request.data, partial=True)
     if serializer.is_valid():
         serializer.save()
-        return Response("update comment success")
+        return Response(comment)
     return Response(serializer.errors)
 
 @api_view(['DELETE'])
