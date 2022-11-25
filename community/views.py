f872305b31e2cc3713a7923c104a9174aaf66339
dippola <dippolas@gmail.com>
2022-11-25 18:15:12 +0900

    version53

--- community/views.py
+++ community/views.py
@@ -1,6 +1,6 @@
 from rest_framework.decorators import api_view
 from .models import UserModel, MainModel, QnaModel, MainCommentModel, QnaCommentModel
-from .serializers import UserModel_serializer, UserModel_Notification_serializer, MainModel_serializer, QnaModel_serializer, MainCommentModel_serializer, QnaCommentModel_serializer
+from .serializers import UserModel_serializer, MainModel_serializer, QnaModel_serializer, MainCommentModel_serializer, QnaCommentModel_serializer
 from django.core.paginator import Paginator
 
 from rest_framework.response import Response
