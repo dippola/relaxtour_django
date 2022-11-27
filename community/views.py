012407a4b4f0fce1f8f286367412d57646a66706
dippola <dippolas@gmail.com>
2022-11-27 19:34:56 +0900

    version68

--- community/views.py
+++ community/views.py
@@ -110,12 +110,12 @@ def getMainsPage(request, page):
     postview = []
     for i in page_obj:
         model = MainModelView(
-            parent_user=page_obj(i)['parent_user'],
-            date=page_obj(i)['date'],
-            title=page_obj(i)['title'],
-            imageurl=page_obj(i)['imageurl'],
+            parent_user=page_obj[i, 'parent_user'],
+            date=page_obj[i, 'date'],
+            title=page_obj[i, 'title'],
+            imageurl=page_obj[i, 'imageurl'],
             count=MainCommentModel.objects.filter(parent_id=i.data['parent_id']),
-            like=page_obj(i)['like']
+            like=page_obj[i, 'like']
         )
         postview.append(model)
     serializer = MainModel_serializer(postview, many=True)
