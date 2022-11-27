9cb82607e7ec33eacf52c9a5c8cf86130b0f3b20
dippola <dippolas@gmail.com>
2022-11-27 20:56:03 +0900

    version91

--- community/views.py
+++ community/views.py
@@ -109,6 +109,7 @@ def getMainsPage(request, page):
     page_obj = paginator.page(page)
     postview = []
     for i in page_obj:
+        print(MainCommentModel.objects.filter(parent_id=i.id).first().count())
         model = MainModelView(
             parent_id=i.id,
             parent_user=i.parent_user,
