1f35c1afc3d0b835df568b24e5f8900584a399d1
dippola <dippolas@gmail.com>
2022-11-28 22:27:09 +0900

    version117

--- community/views.py
+++ community/views.py
@@ -141,7 +141,7 @@ def getMainDetail(request, pk):
         view=post.view,
         like=post.like,
         list=post.list,
-        comment=commentModel
+        comment=set(commentModel)
         # comment=MainCommentModel.objects.filter(parent_id=post.id)
     )
     postDetail.append(model)
