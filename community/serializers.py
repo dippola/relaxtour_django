524dfee074c79b74ca2028a0bb9dfe574815a6a5
dippola <dippolas@gmail.com>
2022-11-29 14:46:29 +0900

    version158

--- community/serializers.py
+++ community/serializers.py
@@ -16,8 +16,13 @@ class MainModelDetail_serializer(serializers.ModelSerializer):
     class Meta:
         model = MainModelDetail
         fields = '__all__'
-
+class MainCommentModel_serializer(serializers.ModelSerializer):
+    # comments = Comment_model_serializer(many=True)
+    class Meta:
+        model = MainCommentModel
+        fields = '__all__'
 class MainModel_serializer(serializers.ModelSerializer):
+    comment = MainCommentModel_serializer(many=True)
     class Meta:
         model = MainModel
         fields = '__all__'
@@ -29,11 +34,7 @@ class QnaModel_serializer(serializers.ModelSerializer):
         model = QnaModel
         fields = '__all__'
 
-class MainCommentModel_serializer(serializers.ModelSerializer):
-    # comments = Comment_model_serializer(many=True)
-    class Meta:
-        model = MainCommentModel
-        fields = '__all__'
+
 
 class QnaCommentModel_serializer(serializers.ModelSerializer):
     # comments = Comment_model_serializer(many=True)
