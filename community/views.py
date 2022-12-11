40957251c5552e285e17d08480ea304b2da08e5d
dippola <dippolas@gmail.com>
2022-12-11 19:43:03 +0900

    version372inpc

--- community/views.py
+++ community/views.py
@@ -129,6 +129,7 @@ def getPostsPageAll(request, page):
             imgcount = len(i.imageurl.split("●"))
         else:
             imgcount = 0
+        like_count = LikeModel.objects.get(parent_id=i.id).user_ids.count()
         model = PostModelView(
             parent_id=i.id,
             parent_user=i.parent_user.id,
@@ -141,7 +142,7 @@ def getPostsPageAll(request, page):
             imageurl=i.imageurl,
             commentcount=PostCommentModel.objects.filter(parent_id=i.id).count(),
             view=i.view,
-            like=LikeModel.objects.get(parent_id=i.parent_user.id).user_ids.count()
+            like=like_count
         )
         postview.append(model)
     serializer = PostModelView_serializer(postview, many=True)
