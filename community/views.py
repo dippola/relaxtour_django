82233756c75cecb11f72fc6a2aa358aa305f1a48
dippola <dippolas@gmail.com>
2022-12-15 17:16:08 +0900

    version422inpc

--- community/views.py
+++ community/views.py
@@ -197,7 +197,7 @@ def getUserCommunityCategory(request, id, category, page):
 @api_view(['GET'])
 def getUsersCommentsAll(request, id, page):
     comments = PostCommentModel.objects.filter(parent_user=id)
-    print(">>>1: " + comments[0].parent_id)
+    print(">>>1: " + str(comments[0]))
     print(">>>2: " + comments[0].parent_id.id)
     page = request.GET.get('page', page)
     paginator = Paginator(comments, 15)
