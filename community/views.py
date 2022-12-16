0851853c514fc6138e3c74b13e83882704fa339d
dippola <dippolas@gmail.com>
2022-12-16 18:10:02 +0900

    version455npc

--- community/views.py
+++ community/views.py
@@ -465,6 +465,8 @@ def createPostComment(request, pk, id):
         to_nickname=to_nickname
     )
     serializer = PostCommentModel_serializer(comment, many=False)
+    print(">>>1: " + str(main.parent_user))
+    print(">>>2: " + str(user.id))
     if main.parent_user != user.id:
         sendNotification(token=main.parent_user.token, title="title", body="body")
     if is_have_to:
