8e6d9037df49bc1ae2c59cc863e7f52f0268f513
dippola <dippolas@gmail.com>
2022-12-16 18:06:46 +0900

    version453npc

--- community/views.py
+++ community/views.py
@@ -466,10 +466,10 @@ def createPostComment(request, pk, id):
     )
     serializer = PostCommentModel_serializer(comment, many=False)
     if main.parent_user != user.id:
-        sendNotification(token=main.parent_user.token, title="title", body="body")
+        sendNotification(request, token=main.parent_user.token, title="title", body="body")
     if is_have_to:
         to_token = UserModel.objects.get(id=to_id).token
-        sendNotification(token=to_token, title="title", body="body")
+        sendNotification(request, token=to_token, title="title", body="body")
     return Response(serializer.data)
 
 @api_view(['GET'])
