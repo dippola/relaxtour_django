c261ca047fe710e2f8877e9d2f670f349e016787
dippola <dippolas@gmail.com>
2022-12-25 20:48:28 +0900

    version520npc

--- community/serializers.py
+++ community/serializers.py
@@ -19,6 +19,7 @@ class PostModel_serializer(serializers.ModelSerializer):
         fields = '__all__'
 
 class LikeModel_serializer(serializers.ModelSerializer):
+    list = serializers.CharField(allow_blank=True)
     class Meta:
         model = LikeModel
         fields = '__all__'
